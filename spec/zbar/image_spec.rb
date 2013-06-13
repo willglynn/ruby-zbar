b295cd5eb02047ba8cf78d2b41dfae3bf3b75185
Will Glynn <will@willglynn.com>
2013-06-13 08:56:54 -0500

    Move process tests to from_pgm context
    This ensures those tests get executed in environments that lack proper
    JPEG support, such as Travis CI.

--- spec/zbar/image_spec.rb
+++ spec/zbar/image_spec.rb
@@ -16,31 +16,6 @@ describe ZBar::Image do
       
         its(:width) { should == 480 }
         its(:height) { should == 240 }
-      
-        describe "process" do
-          it "delegates to the passed processor" do
-            processor = double("processor")
-            expected_result = Object.new
-            processor.should_receive(:process).with(subject).and_return(expected_result)
-          
-            subject.process(processor).should == expected_result
-          end
-
-          it "instantiates a new processor with no arguments" do
-            processor = double("processor")
-            processor.should_receive(:process)
-            ZBar::Processor.should_receive(:new).with().and_return(processor)
-            subject.process
-          end
-
-          it "instantiates a new processor with configuration" do
-            config_hash = { :foo => :bar }
-            processor = double("processor")
-            processor.should_receive(:process)
-            ZBar::Processor.should_receive(:new).with(config_hash).and_return(processor)
-            subject.process(config_hash)
-          end
-        end
       end
     end
   end
@@ -52,9 +27,34 @@ describe ZBar::Image do
     
     context "given test.pgm" do
       let(:pgm_file) { "test.pgm" }
-
+      
       its(:width) { should == 480 }
       its(:height) { should == 240 }
+      
+      describe "process" do
+        it "delegates to the passed processor" do
+          processor = double("processor")
+          expected_result = Object.new
+          processor.should_receive(:process).with(subject).and_return(expected_result)
+        
+          subject.process(processor).should == expected_result
+        end
+
+        it "instantiates a new processor with no arguments" do
+          processor = double("processor")
+          processor.should_receive(:process)
+          ZBar::Processor.should_receive(:new).with().and_return(processor)
+          subject.process
+        end
+
+        it "instantiates a new processor with configuration" do
+          config_hash = { :foo => :bar }
+          processor = double("processor")
+          processor.should_receive(:process)
+          ZBar::Processor.should_receive(:new).with(config_hash).and_return(processor)
+          subject.process(config_hash)
+        end
+      end
     end
   end
 end
